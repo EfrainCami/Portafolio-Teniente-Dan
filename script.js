eaf889db6d1056f0791015bdef93f5b04cf3bae6
EfrainCami <44179029+EfrainCami@users.noreply.github.com>
2023-05-04 20:56:19 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+//funcion q le aplica estilo a la opcion seleccioanda del menu y quita el previemente seleccionado
+function seleccionar(link) {
+    var opciones = document.querySelectorAll('#links  a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
+
+    //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
+    //en modo responsive
+    var x = document.getElementById("nav");
+    x.className = "";
+}
+
+//Mostrar menú responsive
+
+function responsiveMenu(){
+    var x = document.getElementById("nav");
+    if(x.className===""){
+        x.className = "responsive";
+    }else{
+        x.className = "";
+    }
+};
